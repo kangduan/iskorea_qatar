ee3ea7a3a8d155c2b7933c427bfbf522e04ddcf0
kangduan <duanea0606@gmail.com>
2017-01-04 12:25:20 +0000

    dfk

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home/index_qatar'
+  post 'home/write'
+  get 'home/index_qatar'
+  get 'home/about_qatar'
+  get 'home/product_qatar'
+
 end
